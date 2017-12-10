a6d43eac697db23f20e0c13682434d9021e00f7c
Ekaterina Lizogubova <tirli95@gmail.com>
2017-12-10 13:35:42 +0200

    Update lecture18 task

--- lecture18.zen/index.js
+++ lecture18.zen/index.js
@@ -2,37 +2,36 @@
  * Вводим селектор, который существует на странице и нажимаем на одну из кнопок
  * При нажатии на кнопку .find
  * 1. Подсвечивается(используйте класс selected) первый элемент на странице, удовлетворяющий условию
- * 2. Если у элемента есть родитель, то разблокируется кнопка parent
- * 3. Если у элемента есть дети, то разблокируется кнопка child
- * 4. При нажатии на кнопку child убирается выделение с нашего элемента и ставится на ребенка.
+ * 2. Если у элемента есть родители/дети/предыдущие/следующие соседи, то разблокируются соответствующие кнопки(parent, child, previous/next sibling)
+ * 3. Если нет, то они должны остаться заблокрованными
+ * 4. При нажатии на одну из 4х кнопок, убирается выделение с нашего элемента и ставится на соответствующий кнопке.
  *    Теперь он наш активный элемент
- * 5. Тот же принцип с parent
- * 6. Если наш новый элемент не имеет родителя или ребенка, то соответствующие кнопки блокируются :)
+ * 5. При новом поиске старые выделения должны быть удалены.
  *
  * При нажатии на кнопку find all
  * 1. Подсвечиваются все элементы, удовлетворяющие селектору
- * 2. Кнопки parent, child - не доступны
+ * 2. 4 кнопки навигации не доступны
  */
 
-const findBtn = document.querySelector(".find");
-const findAllBtn = document.querySelector(".find-all");
-const childBtn = document.querySelector(".child");
-const parentBtn = document.querySelector(".parent");
-const inputElement = document.querySelector("#selector-finder");
+const findBtn = document.querySelector('.find');
+const findAllBtn = document.querySelector('.find-all');
+const childBtn = document.querySelector('.child');
+const parentBtn = document.querySelector('.parent');
+const inputElement = document.querySelector('#selector-finder');
 
 let element;
 
 function setActiveElement(elem) {
-  elem.classList.add("selected");
+  elem.classList.add('selected');
   childBtn.disabled = !elem.firstElementChild;
   parentBtn.disabled = !elem.parentElement;
 }
 
 function resetSelections() {
-  const elements = document.querySelectorAll(".selected");
+  const elements = document.querySelectorAll('.selected');
 
   elements.forEach(element => {
-    element.classList.remove("selected");
+    element.classList.remove('selected');
   });
 }
 
@@ -41,7 +40,7 @@ function onFindOneClick() {
 
   element = document.querySelector(inputElement.value);
   if (!element) {
-    alert("Incorrect selector");
+    alert('Incorrect selector');
     return;
   }
 
@@ -67,13 +66,13 @@ function onFindAllClick() {
 
 function onFindParentClick() {
   if (element) {
-    element.classList.remove("selected");
+    element.classList.remove('selected');
   }
 
   element = element.parentElement;
   setActiveElement(element);
 }
 
-findBtn.addEventListener("click", onFindOneClick);
-findAllBtn.addEventListener("click", onFindAllClick);
-parentBtn.addEventListener("click", onFindParentClick);
+findBtn.addEventListener('click', onFindOneClick);
+findAllBtn.addEventListener('click', onFindAllClick);
+parentBtn.addEventListener('click', onFindParentClick);
